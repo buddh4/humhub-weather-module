711bed0a64cd9e3f1e5b25d70779906652fbd0f9
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-06-02 09:29:52 +0900

    Update weatherframe.php

--- widgets/views/weatherframe.php
+++ widgets/views/weatherframe.php
@@ -10,13 +10,14 @@ use humhub\models\Setting;
 <div class="panel panel-default panel-weather" id="panel-weather">
     <?= \humhub\widgets\PanelMenu::widget(['id' => 'panel-weather']); ?>
   <div class="panel-heading">
-    <i class="fa fa-cloud">&nbsp;</i><?= Yii::t('WeatherModule.base', '<strong>Weather</strong>') ?>
+    <i class="fa fa-cloud">&nbsp;</i><?= Yii::t('WeatherModule.base', '<strong>Weather</strong>'); ?>
   </div>
   <div class="panel-body">
 
 <?= Html::beginTag('div') ?>
 <a class="weatherwidget-io" href="<?= $weatherUrl ?>" data-label_2="WEATHER" data-font="Roboto" data-icons="Climacons Animated" data-theme="weather_one" ></a>
 <script>
+$(document).on('ready pjax:success', function() { __weatherwidget_init() });
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 </script>
 <?= Html::endTag('div'); ?>
