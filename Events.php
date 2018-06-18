5f7445a3fd4941ad7622c2d6de84700adfd45d01
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-06-19 01:47:41 +0900

    Update Events.php

--- Events.php
+++ Events.php
@@ -12,14 +12,14 @@ class Events extends \yii\base\Object
 
     public static function onAdminMenuInit(\yii\base\Event $event)
     {
-        $event->sender->addItem([
-            'label' => 'Weather Settings',
-            'url' => Url::toRoute('/weather/admin/index'),
-            'group' => 'settings',
-            'icon' => '<i class="fa fa-cloud"></i>',
-            'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'weather' && Yii::$app->controller->id == 'admin',
-            'sortOrder' => 650
-        ]);
+        //$event->sender->addItem([
+        //    'label' => Yii::t('WeatherModule.base', 'Weather Settings'),
+        //    'url' => Url::toRoute('/weather/admin/index'),
+        //    'group' => 'settings',
+        //    'icon' => '<i class="fa fa-cloud"></i>',
+        //    'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'weather' && Yii::$app->controller->id == 'admin',
+        //    'sortOrder' => 650
+        //]);
     }
 
 public static function addWeatherFrame($event)
