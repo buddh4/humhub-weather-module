2166c1e064fe0e72122782d52d11577393e41c0f
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-06-19 01:48:24 +0900

    Update base.php

--- messages/en-US/base.php
+++ messages/en-US/base.php
@@ -1,7 +1,7 @@
 <?php
     return [
         '<strong>Weather</strong>' => '<strong>Weather</strong> ',
-        'Forecast7 Weather Settings' => 'Forecast7 Weather Settings',
+        'Weather Settings' => 'Weather Settings',
         'Weather URL:' => 'Weather URL:',
         '<strong>Weather</strong> module configuration' => '<strong>Weather</strong> module configuration',
         'Save' => 'Save',
