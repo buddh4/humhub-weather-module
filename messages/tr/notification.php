82be73d7910034cb0c8c531270cb152cf1c8fff2
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-03-25 20:22:42 -0400

    Create notification.php

---
+++ messages/tr/notification.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+        'There is an update for ({moduleId}) ({version}) available.' => '',
+        'Receive Notifications for administrative events like available updates' => '',
+        'Administrative' => '',
+
+    ];
