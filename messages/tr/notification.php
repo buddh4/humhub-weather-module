7060ec2654cfa087e6c0d74a99febd327c250ee3
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-03-29 00:36:24 -0400

    Update notification.php

--- messages/tr/notification.php
+++ messages/tr/notification.php
@@ -2,7 +2,7 @@
 
     return [
         'There is an update for the Weather module! ({version})' => '',
-        'Receive Notifications for administrative events like available updates' => '',
-        'Administrative' => '',
+        'Receive Notifications new versions of the Weather module' => '',
+        'Weather Module Updates' => '',
 
     ];
