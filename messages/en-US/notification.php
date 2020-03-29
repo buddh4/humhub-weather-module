795506f8809232da61b1cb40687295688653ab1b
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-03-29 00:35:58 -0400

    Update notification.php

--- messages/en-US/notification.php
+++ messages/en-US/notification.php
@@ -2,7 +2,7 @@
 
     return [
         'There is an update for the Weather module! ({version})' => 'There is an update for the Weather module! ({version})',
-        'Receive Notifications for administrative events like available updates' => 'Receive Notifications for administrative events like available updates',
-        'Administrative' => 'Administrative',
+        'Receive Notifications new versions of the Weather module' => 'Receive Notifications new versions of the Weather module',
+        'Weather Module Updates' => 'Weather Module Updates',
 
     ];
