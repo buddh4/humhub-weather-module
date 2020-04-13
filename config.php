712d0ea3e20336d91f326c7ec24e00683ee0cd0f
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-13 14:27:04 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace humhub\modules\weather;
+namespace gm\modules\weather;
 
-use humhub\modules\weather\Module;
-use humhub\modules\weather\Events;
-use humhub\commands\CronController;
+use gm\modules\weather\Module;
+use gm\modules\weather\Events;
 use humhub\modules\admin\widgets\AdminMenu;
 use humhub\modules\dashboard\widgets\Sidebar;
 use humhub\modules\space\widgets\Sidebar as Spacebar;
@@ -17,7 +16,6 @@ return [
         ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => [Events::class, 'addWeatherFrame']],
         ['class' => Spacebar::class, 'event' => Spacebar::EVENT_INIT, 'callback' => [Events::class, 'addWeatherFrame']],
         ['class' => AdminMenu::class, 'event' => AdminMenu::EVENT_INIT, 'callback' => [Events::class,'onAdminMenuInit']],
-        ['class' => CronController::class, 'event' => CronController::EVENT_ON_DAILY_RUN, 'callback' => [Events::class, 'onCronDailyRun']]
     ]
 ];
 ?>
