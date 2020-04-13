8a03ae7703c821199205c8e5aeae87dbff7763b5
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-13 14:25:01 -0400

    Delete notification.php

--- messages/tr/notification.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    return [
-        'There is an update for the Weather module! ({version})' => '',
-        'Receive Notifications new versions of the Weather module' => '',
-        'Weather Module Updates' => '',
-
-    ];
